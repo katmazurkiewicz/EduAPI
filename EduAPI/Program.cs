8ab28d5dfdf3fcad52aaf21360d79a221a4df976
Katarzyna Mazurkiewicz <50406842+katmazurkiewicz@users.noreply.github.com>
2022-08-08 23:08:59 +0200

    Add AutoMapper & Swashbuckle.Annotations to slnn

--- EduAPI/Program.cs
+++ EduAPI/Program.cs
@@ -1,5 +1,10 @@
+using AutoMapper;
 using EduAPI.Data.Context;
-using EduAPI.Extensions;
+using EduAPI.Data.DAL;
+using EduAPI.Data.DAL.Interfaces;
+using EduAPI.Services;
+using EduAPI.Services.Interfaces;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,9 +12,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddSqlServer<EduContext>(builder.Configuration.GetConnectionString("EduDB"));
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+//builder.Services.AddScoped<IMapper, Mapper>();
+builder.Services.AddScoped<IAuthorService, AuthorService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(Assembly.Load("EduAPI.Services"));
+//builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddSwaggerGen(c => { c.EnableAnnotations(); });
 
 var app = builder.Build();
 
@@ -18,6 +28,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
